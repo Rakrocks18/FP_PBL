d94f72728f88a34d089d724a52dc282e110958e0
Rakrocks18 <119413185+Rakrocks18@users.noreply.github.com>
2023-05-15 00:30:14 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,13 @@
+d1={"Sweet chilli chicken wrap":80,"salad wrap":70,"chicken caesar wrap":80,"chicken mayo wrap":90,"chicken salad wrap":90,"mexican wrap":90,"chicken barbeque wrap":100}
+d2={"salad bread roll":50,"egg and lettuce":60,"chicken and mayo bread roll":60,"hot and spicy chicken schnitzel bread roll":70,"hot chicken gravy bread roll":70}
+d3={"cheese and tomato sandwich":40,"chicken and cheese":50,"tuna sandwich":60,"chicken lettuce and mayo sandwich":60,"chicken caesar sandwich":70,"vegan sandwich":55}
+d4 = {"Chicken Burger": 120, "Veg Burger": 100, "Fries": 60, "Aloo Tikki Burger": 90,"Spicy Chicken Burger": 130, "Spicy Veg Burger": 110}
+d5 = {"Butter Chicken Rice": 150, "Pasta Red Sauce": 140, "Pasta White Sauce": 140, "Roasted Veggies Pasta": 120, "Fried Rice": 90, "Veg Kebab": 70, "Harabhara Kebab": 80}
+d6 = {"Fruit Bowl": 40, "Muffins": 30, "Chicken Wrap": 80, "Veg Wrap": 70, "Veg Club Sandwich": 40}
+
+l1 = list(d1.keys())
+l2 = list(d2.keys())
+l3 = list(d3.keys())
+l4 = list(d4.keys())
+l5 = list(d5.keys())
+l6 = list(d6.keys())
